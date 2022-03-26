f85455c984d454f9f8cd1d17ef7e798025cd444c
Seth Díaz <seth261099@gmail.com>
2022-03-26 16:31:09 -0600

    Simplification

--- api/src/index.js
+++ api/src/index.js
@@ -9,14 +9,16 @@ const ChatMessage = require(path.join(__dirname, 'models/chat.js'))
 const port = 1623;
 const dbserver = '192.168.1.154';
 
-var connection = mongoose.createConnection(`mongodb://${dbserver}`)
 var dbList = [];
 const dbExcluded = ['admin', 'config', 'local'];
-connection.on('open', function() {
-    new Admin(connection.db).listDatabases(function(err, result) {
+
+var connection = mongoose.createConnection(`mongodb://${dbserver}`)
+connection.on('open', () => {
+    new Admin(connection.db).listDatabases((err, result) => {
         result.databases.forEach(db =>{
-            // console.log(db.name);
-            dbList.push(db.name)
+            if(!dbExcluded.includes(db.name)){
+                dbList.push(db.name)
+            }
         })
     });
 });
@@ -32,7 +34,7 @@ app.get('/chatlog/:dbName', async (req, res) => {
     var databaseName = req.params.dbName
     var chatlog = []
 
-    if(!dbExcluded.includes(databaseName) && dbList.includes(databaseName)){
+    if(dbList.includes(databaseName)){
         var dbAux = connection.useDb(databaseName)
         var messages = dbAux.model('Message', ChatMessage);
         var chatlog = await messages.find({});
